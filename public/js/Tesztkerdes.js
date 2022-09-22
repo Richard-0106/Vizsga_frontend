312900511e29e6dd7b9db849c7a5d0b6623a78b8
Richárd <alex19950106@gmail.com>
2022-09-22 10:06:47 +0200

    OKJ vizsgafeladat javítva

--- public/js/Tesztkerdes.js
+++ public/js/Tesztkerdes.js
@@ -1,23 +1,43 @@
-class Tesztkerdes{
-    constructor(elem,adat){
-        this.elem=elem
-        this.adat=adat
+class Tesztkerdes {
+    constructor(elem, adat) {
+        this.elem = elem
+        this.adat = adat
+        this.valaszok = [this.adat.v1, this.adat.v2, this.adat.v3, this.adat.v4]
+        this.kattintottId
         this.megjelenit()
-        this.szinvaltas=this.elem.find(".v1")
-        this.szinvaltas.on("click", () => {
-            this.szinValtozasEsemeny()
-        })
+        this.valszok = this.elem.find(".valaszok").children()
+        for (let i = 0; i < this.valszok.length; i++) {
+            $(this.valszok[i]).on("click", () => {
+                this.szinValtozasEsemeny(i)
+            })
+        }
     }
-    megjelenit(){
-        this.elem.append("<div class='kerdes'>"+this.adat.kerdes+"</div>")
-        this.elem.append("<div class='valaszok'>"
-        +"<div class='v1'>"+this.adat.v1+"</div>"
-        +"<div class='v2'>"+this.adat.v2+"</div>"
-        +"<div class='v3'>"+this.adat.v3+"</div>"
-        +"<div class='v3'>"+this.adat.v4+"</div></div>")
+    megjelenit() {
+        this.elem.append("<div class='kerdes'>" + this.adat.kerdes + "</div>")
+        this.elem.append("<div class='valaszok'></div>")
+        this.shuffle()
+        for (let i = 0; i < this.valaszok.length; i++) {
+            $(this.elem.find(".valaszok")).append("<div><p>"+this.valaszok[i]+"</p></div>")
+        }
     }
-    szinValtozasEsemeny() {
-        let esemeny = new CustomEvent("szinValtas", { detail: this.adat })
+    szinValtozasEsemeny(i) {
+        this.kattintottId = i
+        this.kattintottErtek = this.valaszok[i]
+        let esemeny = new CustomEvent("szinValtas", { detail: this })
         window.dispatchEvent(esemeny)
+        for (let i = 0; i < this.valszok.length; i++) {
+            $(this.valszok[i]).unbind()
+        }
+    }
+
+    shuffle() {
+        let currentIndex = this.valaszok.length, randomIndex;
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.valaszok[currentIndex], this.valaszok[randomIndex]] = [
+                this.valaszok[randomIndex], this.valaszok[currentIndex]];
+        }
     }
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,75 @@
-$(function (){
+$(function () {
     const token = $('meta[name="csrf-token"]').attr("content");
-    const ajax = new Ajax(token);
-    ajax.getAjax("/kategoria",kategoriaKiir)
-    ajax.getAjax("/tesztek",tablazatKiir)
+    const ajax = new Ajax(token)
+    var kerdesek = []
+    var kerdesekKategorizalva = []
+    let kerdesIndex = 0
+    ajax.getAjax("/kategoria", kategoriaKiir)
+    ajax.getAjax("/tesztek", kerdesekEltarolasa)
     function kategoriaKiir(tomb) {
         $("#teszt-valaszto").empty()
-        $("#teszt-valaszto").append("<option disable selected>Válassz egy kategoriát</option>")
+        $("#teszt-valaszto").append("<option disabled selected>Válassz egy kategoriát</option>")
         tomb.forEach(element => {
             $("#teszt-valaszto").append("<option id=" + element.id + ">" + element.kategorianev + "</option>")
         });
     }
-    function tablazatKiir(tomb) {
-        console.log(tomb)
-        $("#tesztek").empty()
-        for (let index = 0; index < tomb.length; index++) {
-            const element = tomb[index];
+    function kerdesekEltarolasa(tomb) {
+        for (let i = 0; i < tomb.length; i++) {
+            kerdesek.push(tomb[i])
+            kerdesekKategorizalva.push(tomb[i])
+        }
+        kerdesKiIras()
+    }
+    function kerdesKiIras() {
+        console.log(kerdesekKategorizalva)
+        if (kerdesIndex < kerdesekKategorizalva.length) {
+            $("#tesztek").empty()
+            const element = kerdesekKategorizalva[kerdesIndex];
             const elem = $("<div class='sor '></div>").appendTo("#tesztek")
             new Tesztkerdes(elem, element)
+            kerdesIndex++
         }
-     
     }
+    // function tablazatKiir(tomb) {
+    //     console.log(tomb)
+    //     for (let index = 0; index < tomb.length; index++) {
+    //         const element = tomb[index];
+    //         const elem = $("<div class='sor '></div>").appendTo("#tesztek")
+    //         new Tesztkerdes(elem, element)
+    //     }
+
+    // }
     $(window).on("szinValtas", (esemeny) => {
-       console.log(esemeny.detail.id)
-       $(".v1").css("background","green")
-       $(".v1").css("color","white")
-       $(".v2").attr("disabled", false)
-       $(".v3").attr("disabled", false)
-       $(".v4").attr("disabled", false)
+        const kattintottErtek = esemeny.detail.kattintottErtek
+        if (kattintottErtek == esemeny.detail.adat.helyes) {
+            $(esemeny.detail.valszok[esemeny.detail.kattintottId]).css("background", "green")
+        }
+        else {
+            $(esemeny.detail.valszok[esemeny.detail.kattintottId]).css("background", "red")
+            let i = 0
+            while (i < esemeny.detail.valaszok.length && esemeny.detail.valaszok[i] != esemeny.detail.adat.helyes) {
+                console.log(kattintottErtek, esemeny.detail.adat.helyes)
+                i++
+            }
+            $(esemeny.detail.valszok[i]).css("background", "green")
+        }
+        setTimeout(() => {
+            kerdesKiIras()
+        }, 2000)
+    })
+
+    $("#teszt-valaszto").on("change", function () {
+        let id = $(this)[0].selectedIndex
+        kerdesekKategorizalva = []
+        console.log(kerdesek)
+        let kivalasztottKategoria = $(this)[0][id].text
+        for (let i = 0; i < kerdesek.length; i++) {
+            console.log(kerdesek[i])
+            if (kerdesek[i].kategoria.kategorianev == kivalasztottKategoria) {
+                kerdesekKategorizalva.push(kerdesek[i])
+            }
+        }
+        kerdesIndex = 0
+        kerdesKiIras()
     })
 })
